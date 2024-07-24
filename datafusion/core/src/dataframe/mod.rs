897bd60a24c454af2deb9eba65d11ebd29b6e223
Dhruvil Shah <dhruvilshah@Dhruvils-MBP.attlocal.net>
2024-07-24 09:00:43 -0400

    adding the plan

--- datafusion/core/src/dataframe/mod.rs
+++ datafusion/core/src/dataframe/mod.rs
@@ -891,6 +891,7 @@ impl DataFrame {
     pub async fn collect(self) -> Result<Vec<RecordBatch>> {
         let task_ctx = Arc::new(self.task_ctx());
         let plan = self.create_physical_plan().await?;
+        eprintln!("{:?}", plan );
         let result = collect(plan, task_ctx).await;
 
         return result;
